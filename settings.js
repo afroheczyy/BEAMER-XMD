7700e29286c024245d0bed29af09cb0676fe9edc
afroheczyy <hayfordhector@gmail.com>
2026-06-04 10:26:15 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ownerNumber: [''], // Put your number here e.g. ['2547XXXXXXXX'] MUST
-    sessionID: ''      // Paste your session ID here MUST
+    ownerNumber: ['233249654855'], // Put your number here e.g. ['2547XXXXXXXX'] MUST
+    sessionID: 'blinder~bqpHSKYB#rRg2vKNFk001jAQmQOeYzh9eIxabk5pT0iI56okCEtk'      // Paste your session ID here MUST
 };
